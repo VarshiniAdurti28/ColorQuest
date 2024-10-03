2099940b56a26cfef86fe065f17c47c89ba7dc17
Varshini <varshini.231cs204@nitk.edu.in>
2024-10-03 23:56:18 +0530

    Color Conversions using chroma.js Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,39 @@
 <script>
-  let hexCont, hsvCont, hslCont, cmykCont;
-  let rgbCont= "rgb(0, 0, 0)";
+  //chroma.js library for the conversions
+  import chroma from 'chroma-js';
+
+  let selectedCol ="#4340a3"; 
+  let rgbCol = '', cmykCol='', hslCol='', hsvCol='', hexCol='';
+  
+
+
+ 
+  $: {
+
+  //HEX: Our selected Color is in Hex
+  hexCol= selectedCol;
+
+  //RGB
+  let rgbColor = chroma(selectedCol).rgb(); 
+  rgbCol = `${rgbColor[0]}, ${rgbColor[1]}, ${rgbColor[2]}`;
+
+  //CMYK
+  let cmykColor = chroma(selectedCol).cmyk(); 
+  cmykCol = `${Math.round(cmykColor[0] * 100)}%, ${Math.round(cmykColor[1] * 100)}%, ${Math.round(cmykColor[2] * 100)}%, ${Math.round(cmykColor[3] * 100)}%`;
+  }
+
+   // HSL
+  let hslColor = chroma(selectedCol).hsl(); 
+  hslCol = `${Math.round(hslCol[0])}°, ${Math.round(hslCol[1] * 100)}%, ${Math.round(hslCol[2] * 100)}%`;
+
+  // HSV
+  let hsvColor = chroma(selectedCol).hsv();
+  hsvCol = `${Math.round(hsvColor[0])}°, ${Math.round(hsvColor[1] * 100)}%, ${Math.round(hsvColor[2] * 100)}%`;
 
 </script>
+
+
+
 
 <style>
   .colors{
@@ -32,6 +63,11 @@
 
   .eachInp{
     padding: 20px;
+    
+  }
+
+  input{
+    width: 300px;
   }
 
  label{
@@ -45,7 +81,7 @@
 
 <div class="cont">
   <div class= "colors">
-    <div class= "dispColor" style= "--pick-color: {rgbCont}">
+    <div class= "dispColor" style= "--pick-color: {selectedCol}">
 
     </div>
     <div>
@@ -57,34 +93,34 @@
   <div class="colorbar">
     <div class="eachInp">
       <label> HEX
-        <input type="text" bind:value = {hexCont}>
+        <input type="text" bind:value = {hexCol}>
       </label>
     </div>
     
     <div class="eachInp">
       <label> HSV
-        <input type="text" bind:value = {hsvCont}>
+        <input type="text" bind:value = {hsvCol}>
       </label>
     </div>
     
 
     <div class="eachInp">
       <label> HSL
-        <input type="text" bind:value = {hslCont} >
+        <input type="text" bind:value = {hslCol} >
       </label>
     </div>
     
 
     <div class="eachInp">
     <label> CMYK
-      <input type="text" bind:value = {cmykCont}>
+      <input type="text" bind:value = {cmykCol}>
     </label>
     </div>
 
 
     <div class="eachInp">
     <label> RGB
-      <input type="text" bind:value = {rgbCont}>
+      <input type="text" bind:value = {rgbCol}>
     </label>
 
     </div>
